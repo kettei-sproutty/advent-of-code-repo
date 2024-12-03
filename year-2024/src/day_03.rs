5da503009028c718654237272703b02399d9eb98
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2024-12-03 18:03:47 +0100

    feat(2024-03): part 2, use group in part 1

--- year-2024/src/day_03.rs
+++ year-2024/src/day_03.rs
@@ -1,16 +1,33 @@
 pub use regex::Regex;
 
 pub fn part_one(input: &str) -> isize {
- Regex::new(r"mul\(\d*,\d*\)").unwrap().captures_iter(input).fold(0, |acc, cap| {
-    let mut iter = cap[0].split(",");
-    let a = iter.next().unwrap().replace("mul(", "").parse::<isize>().unwrap();
-    let b = iter.next().unwrap().replace(")", "").parse::<isize>().unwrap();
+ Regex::new(r"mul\((\d+,\d+)\)").unwrap().captures_iter(input).fold(0, |acc, cap| {
+    let mut iter = cap[1].split(",");
+    let a = iter.next().unwrap().parse::<isize>().unwrap();
+    let b = iter.next().unwrap().parse::<isize>().unwrap();
     acc + a * b
   })
 }
 
 pub fn part_two(_input: &str) -> isize {
-  0
+  let mut is_dont = false;
+  Regex::new(r"mul\((\d+,\d+)\)|do\(\)|don't\(\)").unwrap().captures_iter(_input).fold(0, |acc, cap| {
+    let is_modifier = cap.get(1).is_none();
+
+    if is_modifier{
+      is_dont = cap.get(0).unwrap().as_str().contains("don't");
+      return acc;
+    }
+
+    if is_dont {
+      return acc;
+    }
+
+    let mut iter = cap[1].split(",");
+    let a = iter.next().unwrap().parse::<isize>().unwrap();
+    let b = iter.next().unwrap().parse::<isize>().unwrap();
+    acc + a * b
+  })
 }
 
 #[allow(dead_code)]
@@ -38,6 +55,6 @@ mod tests {
   #[test]
   fn test_part_two_example() {
     let example_assets = include_str!("../assets/day-03/example.txt");
-    assert_eq!(part_two(example_assets), 0);
+    assert_eq!(part_two(example_assets), 48);
   }
 }
